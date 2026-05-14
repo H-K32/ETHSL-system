fb5750cbc197fdca9862dd99cda399b17077a6d6
sekina <sekimurad123@gmail.com>
2026-05-14 16:27:31 -0700

    Fix admin + level dropdown + deployment config

--- backend/EthSL/courses/admin.py
+++ backend/EthSL/courses/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Level, Course, Lesson, Quiz, Question, Option
 
-# Register your models here.
+
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "order")
+    ordering = ("order",)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "level")
+    list_filter = ("level",)
+    search_fields = ("title",)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "course", "order")
+    ordering = ("course", "order")
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ("id", "quiz_type", "level", "course", "lesson")
+    list_filter = ("quiz_type",)
