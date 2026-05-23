596b6fd1c5a9729bd5503e77c0191937f77832c8
sekina <sekimurad123@gmail.com>
2026-05-23 03:18:27 -0700

    add

--- backend/EthSL/users/views.py
+++ backend/EthSL/users/views.py
@@ -10,6 +10,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from progress.models import LessonProgress
 from community.models import Report
+from rest_framework.permissions import AllowAny
  
 from .password_serializers import (
     PasswordResetRequestSerializer,
