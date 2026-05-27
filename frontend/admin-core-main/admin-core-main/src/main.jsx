7d69dced0f64b1e018ed18edf39434de24463ad4
sekina <sekimurad123@gmail.com>
2026-05-27 16:57:50 -0700

    main

--- frontend/admin-core-main/admin-core-main/src/main.jsx
+++ frontend/admin-core-main/admin-core-main/src/main.jsx
@@ -3,3 +3,4 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(<App />);
+console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL)
--- frontend/lms/src/main.jsx
+++ frontend/lms/src/main.jsx
@@ -13,4 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
+
+  console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL)
 )
