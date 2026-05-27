003416ac1933800028f8613dc2f5ab554a25e3c7
lud <ludamare3@gmail.com>
2026-05-28 01:43:09 +0300

    forgot password and reset password CSS

--- frontend/admin-core-main/admin-core-main/src/pages/AdminResetPassword.jsx
+++ frontend/admin-core-main/admin-core-main/src/pages/AdminResetPassword.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import API from "../api/axiosConfig";
+import "../styles/adminresetpassword.css";
 
 function AdminResetPassword() {
 
