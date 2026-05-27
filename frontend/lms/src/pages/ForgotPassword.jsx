527fc2d264f3424070a83c9e731e9f74fd998263
lud <ludamare3@gmail.com>
2026-05-28 02:47:04 +0300

    learner forgot password UI

--- frontend/lms/src/pages/ForgotPassword.jsx
+++ frontend/lms/src/pages/ForgotPassword.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import axios from "axios"
+import "../styles/forgotpassword.css"
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("")
