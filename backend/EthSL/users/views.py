ffaa8830fff07e32cd9f5adf5507962928375c66
sekina <sekimurad123@gmail.com>
2026-05-28 14:38:35 -0700

    send email2

--- backend/EthSL/users/views.py
+++ backend/EthSL/users/views.py
@@ -15,7 +15,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_str
-#from django.core.mail import send_mail
+from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils.http import urlsafe_base64_encode
